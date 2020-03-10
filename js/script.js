526e8b2fb7ae0f05a36d2177821033c8a388eaf7
Alan Ashworth <awashworth927@gmail.com>
2020-03-10 16:25:37 -0400

    - Add question sets to js file.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var questionSet = [
+  {
+    questionNo: 1,
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    questionNo: 2,
+    question:
+      "The condition in an if / else statement is enclosed within ______.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    questionNo: 3,
+    question: "rrays in JavaScript can be used to store ______.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "parentheses"
+  },
+  {
+    questionNo: 4,
+    question:
+      "String values must be closed within ______ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "parentheses"
+  },
+  {
+    questionNo: 5,
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console log"],
+    answer: "parentheses"
+  }
+];
