d0d1132edc0c0c31e9a016c5d29fcd292e9073c7
Alan Ashworth <awashworth927@gmail.com>
2020-03-11 19:24:44 -0400

    - Removed unused object.

--- js/script.js
+++ js/script.js
@@ -100,11 +100,6 @@ function stopCountDownTimer(){
 }
 
 // Objects --------------------------------------------------------------------
-var challengePane = {
-  introduction:
-    "To answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
-};
-
 var questions = [
   {
     question: "Commonly used data types DO NOT include:",
